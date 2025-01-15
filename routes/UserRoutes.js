2e7ea3b35be25a7ddf163cafce6326602736b4e9
Alby <alby4645@gmail.com>
2025-01-15 21:37:48 +0530

    4 commit

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -70,4 +70,22 @@ router.get('/user-data', verifyToken, async (req, res) => {
   }
 });
 
+router.get('/fetch-by-id/:id', async (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+
+  try {
+    const user = await User.findById(id);
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found.' });
+    }
+
+    return res.status(200).json({ message: 'User data retrieved successfully.', user });
+  } catch (error) {
+    console.error('Error fetching user by ID:', error);
+    return res.status(500).json({ message: 'An error occurred while fetching user data.', error });
+  }
+});
+
 module.exports = router;
